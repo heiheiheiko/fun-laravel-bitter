c314668925492b479b839b8c30eca6dcbddb9233
Heiko Beineke <heiko.beineke@gmail.com>
2021-10-17 00:36:51 +0200

    Add fontawesome

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue';
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSkull } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 
 const app = createApp(App);
+
+library.add(faSkull);
 
 const requireComponent = require.context(
   './components',
@@ -20,6 +25,8 @@ requireComponent.keys().forEach((fileName) => {
 
   app.component(componentName, componentConfig.default || componentConfig);
 });
+
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 
 app.mount('#app');
 
